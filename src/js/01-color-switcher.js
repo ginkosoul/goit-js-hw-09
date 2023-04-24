341c72e6b79b13ac508682acf1234e22cd9e9fda
ginkosoul <ginkosoul@gmail.com>
2023-04-24 16:36:43 +0300

    completed first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,44 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+const CHANGECOLOR_TIME = 1000;
+let changeBackgroundId;
+
+// console.log(startBtn);
+// console.log(stopBtn);
+
+startBtn.addEventListener('click',onStartBtnClick)
+
+
+
+function onStartBtnClick(e){
+    console.log(startBtn.disabled);
+    startBtn.disabled = true;
+    startBtn.removeEventListener('click',onStartBtnClick)
+
+    stopBtn.addEventListener('click', onStopBtnClick)
+    stopBtn.disabled = false;
+
+    changeBackgroundId = setInterval(changeBackground,CHANGECOLOR_TIME);
+}
+
+function onStopBtnClick(e){
+
+    stopBtn.disabled = true;
+    stopBtn.removeEventListener('click', onStopBtnClick)
+
+    startBtn.addEventListener('click',onStartBtnClick)
+    startBtn.disabled = false;
+
+    clearInterval(changeBackgroundId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+function changeBackground(){
+    body.style.backgroundColor = getRandomHexColor();
+    // console.log(body.style.backgroundColor);
+}
