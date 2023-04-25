d6c28fcfcf0d96b1d31197a68b1edac697dbcf3d
ginkosoul <ginkosoul@gmail.com>
2023-04-25 21:34:21 +0300

    Update promises. added new function

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,39 +5,33 @@ const refs = {
   step: document.querySelector('input[name="step"]'),
   amount: document.querySelector('input[name="amount"]'),
 }
+Notify.init({
+  fontSize: '13px',
+  clickToClose: true,
+});
 
-let count = 0;
-let countId = null;
 
 // console.log(refs);
 refs.btnCreatePromices.addEventListener('click', onClick)
 
 function onClick(event) {
   event.preventDefault();
-  const stepValue = refs.step.value;
-  const delayValue = refs.delay.value;
-  count = Number(refs.amount.value);
+  const stepValue = Number(refs.step.value);
+  const delayValue = Number(refs.delay.value);
+  const count = Number(refs.amount.value);
   // console.log("onClick body",count,delayValue,stepValue);
-
-  countId = setInterval(() => {
-    // console.log("Interval body");
-    createPromise(count, delayValue)
-      .then(({ position, delay }) => {
-        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
-        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
-        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-
-    count -= 1;
-    if (count < 1){
-      clearInterval(countId);
-      // console.log(countId);
-    }
-  }, stepValue)
-  event.currentTarget.closest('form').reset();
+  if (stepValue < 0 || delayValue < 0 || count < 1) {
+    Notify.warning('Please enter valid value');
+    return;
+  }
+  runPromises(delayValue, stepValue, count)
+  // event.currentTarget.closest('form').reset();
+  // console.log(event.currentTarget.disabled);
+  refs.btnCreatePromices.disabled = true;
+  // console.log("Кнопка вимкнена на ", delayValue + stepValue*(count - 1));
+  setTimeout(() => {
+    refs.btnCreatePromices.disabled = false;
+  }, delayValue + stepValue*(count - 1))
 }
 
 function createPromise(position, delay) {
@@ -56,4 +50,20 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
+}
+
+function runPromises(firstDeelay, delay, count){
+  if (count > 0) {
+    runPromises(firstDeelay, delay, count - 1)
+    // console.log(count);
+    createPromise(count, firstDeelay + delay*(count - 1))
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+        // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
 }
